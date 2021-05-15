47830014b14073827f5f526a2ad4fa843f009300
kiwioGIT <pvl101@seznam.cz>
2021-05-15 16:22:47 +0200

    asd

--- dungeon_genaration/TileMap.gd
+++ dungeon_genaration/TileMap.gd
@@ -4,52 +4,56 @@ extends TileMap
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var closest_dist
-var closest_vec
+#var closest_dist
+#var closest_vec
 var mids = []
 var red_mids = []
-var room_amount = 12
-var room_max_size = 10
-var room_min_size = 3
-var room_max_pos_x = 40
-var room_max_pos_y = 25
+var room_amount = 100
+var room_max_size = 15
+var room_min_size = 5
+var room_max_pos_x = 80
+var room_max_pos_y = 50
 var blue_mids = []
-var sizes = []
-var poses = []
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in room_amount:
 		randomize()
+		var yadition = 0
+		var iscoliding = true
 		var sizex = int(rand_range(room_min_size,room_max_size))
 		var sizey = int(rand_range(room_min_size,room_max_size))
-		sizes.append(Vector2(sizex,sizey))
 		var posx = int(rand_range(-room_max_pos_x,room_max_pos_x))
 		var posy = int(rand_range(-room_max_pos_y,room_max_pos_y))
-		
-		poses.append(Vector2(posx,posy))
+		while(iscoliding):
+			for x in sizex+2:
+				for y in sizey+2:
+					iscoliding = false
+					if get_cellv(Vector2(posx+x-1,posy+y+yadition-1))==0:
+						iscoliding = true
+						yadition += 2
 		for x in sizex:
 			for y in sizey:
-				if get_cellv(Vector2(posx+x,posy+y))==-1:
-					set_cellv(Vector2(posx+x,posy+y),0)
+				if get_cellv(Vector2(posx+x,posy+y+yadition))==-1:
+					set_cellv(Vector2(posx+x,posy+y+yadition),0)
 				else:
-					set_cellv(Vector2(posx+x,posy+y),-1)
+					set_cellv(Vector2(posx+x,posy+y+yadition),0)
 				if posx+x == posx+int(sizex/2) and posy+y == posy+int(sizey/2):
-					mids.append(Vector2(posx+x,posy+y))
+					mids.append(Vector2(posx+x,posy+y+yadition))
+		print("y add ",yadition)
+
 	print(mids)
-	for mid_pos in mids:
-		var closest_dist
-		var closest_vec
-		for mid_pos2 in mids:
-				if (closest_dist == null or get_distancev(mid_pos,mid_pos2) < closest_dist) and mid_pos != mid_pos2:
-					closest_dist = get_distancev(mid_pos,mid_pos2)
-					closest_vec = mid_pos2
-		connect_with_tiles(mid_pos,closest_vec,0)
 	var mid_to_turn_red = int(rand_range(0,room_amount))
 	set_cellv(mids[mid_to_turn_red],1)
 	set_blue_mids()
+	for i in 10:
+		delete_badly_placed_tiles()
 	while(blue_mids.size() > 0):
-		connect_pieces()
+		connect_closest_pieces()
+	for i in 10:
+		post_delete_badly_placed_tiles()
+
 
 func fill_red_from_bot():
 	var curent_red_check = Vector2(room_max_pos_x*2,room_max_pos_y*2)
@@ -67,9 +71,74 @@ func connect_with_tiles(start,finish,tile):
 		connect_with_tiles_hv(start,finish,tile)
 	else:
 		connect_with_tiles_vh(start,finish,tile)
-	
 
-func connect_pieces():
+
+func delete_badly_placed_tiles():
+	var curent_check = Vector2(-room_max_pos_x*2,-room_max_pos_y*2)
+	for ch in room_max_pos_x*room_max_pos_y*16:
+		var spaces_around = 0
+		if get_cellv(Vector2(curent_check.x-1,curent_check.y)) == 0 and get_cellv(Vector2(curent_check.x,curent_check.y-1)) == 0 and get_cellv(Vector2(curent_check.x-1,curent_check.y-1)) == -1:
+			set_cellv(Vector2(curent_check.x-1,curent_check.y),-1)
+			
+		if get_cellv(Vector2(curent_check.x+1,curent_check.y)) == 0 and get_cellv(Vector2(curent_check.x,curent_check.y-1)) == 0 and get_cellv(Vector2(curent_check.x+1,curent_check.y-1)) == -1:
+			set_cellv(Vector2(curent_check.x+1,curent_check.y),-1)
+			
+		if get_cellv(Vector2(curent_check.x-1,curent_check.y)) == 0 and get_cellv(Vector2(curent_check.x,curent_check.y+1)) == 0 and get_cellv(Vector2(curent_check.x-1,curent_check.y+1)) == -1:
+			set_cellv(Vector2(curent_check.x-1,curent_check.y),-1)
+			
+		if get_cellv(Vector2(curent_check.x+1,curent_check.y)) == 0 and get_cellv(Vector2(curent_check.x,curent_check.y+1)) == 0 and get_cellv(Vector2(curent_check.x+1,curent_check.y+1)) == -1:
+			set_cellv(Vector2(curent_check.x+1,curent_check.y),-1)
+		
+		if get_cellv(Vector2(curent_check.x+1,curent_check.y))==-1:
+			spaces_around+=1
+		if get_cellv(Vector2(curent_check.x-1,curent_check.y))==-1:
+			spaces_around+=1
+		if get_cellv(Vector2(curent_check.x,curent_check.y+1))==-1:
+			spaces_around+=1
+		if get_cellv(Vector2(curent_check.x,curent_check.y-1))==-1:
+			spaces_around+=1
+		if spaces_around >= 3:
+			 set_cellv(curent_check,-1)
+
+func post_delete_badly_placed_tiles():
+	var curent_check = Vector2(-room_max_pos_x*2,-room_max_pos_y*2)
+	for ch in room_max_pos_x*room_max_pos_y*16:
+		var spaces_around = 0
+		
+		if get_cellv(Vector2(curent_check.x+1,curent_check.y))==-1:
+			spaces_around+=1
+		if get_cellv(Vector2(curent_check.x-1,curent_check.y))==-1:
+			spaces_around+=1
+		if get_cellv(Vector2(curent_check.x,curent_check.y+1))==-1:
+			spaces_around+=1
+		if get_cellv(Vector2(curent_check.x,curent_check.y-1))==-1:
+			spaces_around+=1
+		if spaces_around >= 3:
+			 set_cellv(curent_check,-1)
+
+func connect_closest_pieces():
+	fill_red()
+	fill_red_from_bot()
+	fill_red()
+	fill_red_from_bot()
+	fill_red()
+	fill_red_from_bot()
+	set_red_mids()
+	set_blue_mids()
+	randomize()
+	var c_distance = 9999999999
+	var crmid
+	var cbmid
+	for rmid in red_mids:
+		for bmid in blue_mids:
+			if get_distancev(rmid,bmid) < c_distance:
+				c_distance = get_distancev(rmid,bmid)
+				crmid = rmid
+				cbmid = bmid
+	if blue_mids.size() > 0:
+		connect_with_tiles(crmid,cbmid,1)
+
+func connect_random_pieces():
 	fill_red()
 	fill_red_from_bot()
 	fill_red()
@@ -83,8 +152,7 @@ func connect_pieces():
 	var rand_blue_mid = int(rand_range(0,blue_mids.size()))
 	if blue_mids.size() > 0:
 		connect_with_tiles(red_mids[rand_red_mid],blue_mids[rand_blue_mid],1)
-	print("asdasdasd",red_mids[rand_red_mid])
-	
+
 func fill_red():
 	var curent_red_check = Vector2(-room_max_pos_x*2,-room_max_pos_y*2)
 	for ch in room_max_pos_x*room_max_pos_y*16:
@@ -101,14 +169,13 @@ func set_red_mids():
 	for m in mids:
 		if get_cellv(m)==1:
 			red_mids.append(m)
-	print("red mids ",red_mids)
 
 func set_blue_mids():
 	blue_mids = []
 	for m in mids:
 		if get_cellv(m)==0:
 			blue_mids.append(m)
-	print("blue mids ",red_mids)
+
 
 func get_distancev(vector_1,vector_2):
 	var f = vector_1-vector_2
